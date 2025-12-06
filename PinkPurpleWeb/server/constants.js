1636647b362662648deb8bf3da83b1035ca2a2ea
Helron1977 <47913777+Helron1977@users.noreply.github.com>
2025-12-06 18:17:17 +0100

    adjust time ratio constant

--- PinkPurpleWeb/server/constants.js
+++ PinkPurpleWeb/server/constants.js
@@ -2,7 +2,7 @@
 const WIDTH = 1920;
 const HEIGHT = 1080;
 const GRAVITY = 8;
-const T_INC = 0.0166; // Time increment per frame (~1/60s for real-time simulation)
+const T_INC = 0.2; // 0.3 too fast - 0.016 too slow
 const R_MIN = 25;
 const R_MAX = 30;
 
